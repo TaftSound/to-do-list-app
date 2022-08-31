2cad960bb7a1e68b2c23778503a1bb0aef169dc3
TaftSound <mattmurphytattoos@gmail.com>
2022-08-31 12:54:45 -0700

    Style task and category section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import "./style.css"
-import manipulateDOM from "./modules/DOM-manipulation.mjs"
+import './style.css'
+import manipulateDOM from './modules/DOM-manipulation.mjs'
+import taskData from './modules/todo-data.mjs'
 
 manipulateDOM.createPageStructure()
+taskData.addCategory('Work', 'Do some of it brah')
+taskData.addTask(
+  'Do da hose down da gardin',
+  'feb 25',
+  'some notes',
+  'schmoop',
+  'more schmmop',
+  'actually even more schmoop'
+)
+manipulateDOM.displayTask('Do some works', '09/01/2022')
+manipulateDOM.displayTask('Do some other works', '09/02/2022')
+manipulateDOM.displayCurrentCategory('Work Stuff')
